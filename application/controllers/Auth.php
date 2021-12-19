b2491532f6f4d64de5551ecd175753f43cfc4117
iliasoulddahi <iliasoulddahi@gmail.com>
2021-12-19 23:13:28 +0700

    testgit

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,6 +3,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
+    ///////////////////////////////////////////
+    //////////////////////////////////////////
 
     public function __construct()
     {
